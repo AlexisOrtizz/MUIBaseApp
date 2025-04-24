6c322f5e39d4e4f3f158fa5eaa299cc381bda32d
Alexis Ortiz <olmedo.aortiz@gmail.com>
2025-04-23 23:01:16 -0300

    [MOD] Modify proyect configs.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import AuthLayout from '@/layouts/AuthLayout';
+import { AppRouter } from './routes/AppRouter';
+
+export default function App() {
+  return (
+    // <AuthLayout />
+    <AppRouter />
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import SignInSide from './SignInSide.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import MuiApp from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SignInSide />
+    <BrowserRouter>
+      <MuiApp />
+    </BrowserRouter>
   </StrictMode>,
 )
